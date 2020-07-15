dfc14be6202074843da73d8a20be2e395cfe839b
cristian <cra.bustamante@alumnos.duoc.cl>
2020-07-14 23:29:55 -0400

    agregando mas cosas a las funciones

--- funciones/index.php
+++ funciones/index.php
@@ -20,4 +20,29 @@ if (isset($_GET['numero'])) {
 } else {
     echo "<h1>debe contener algun numero</h1>";
 }
+echo "///////////////////////////////////////"."<br>";
 
+function Calculadora($num, $num2, $negrita=false) {
+   $suma = $num + $num2;
+   $resta = $num - $num2;
+   $multiplcacion = $num*$num2;
+   $division = $num/$num2;
+   
+   if ($negrita) {
+       echo "<h1>";
+       
+   }
+   echo "Suma:".$suma."<br>";
+   echo "resta:".$resta."<br>";
+   echo "division:".$division."<br>";
+   echo "multiplicacion:".$multiplcacion."<br>";
+   
+   if ($negrita) {
+       echo "</h1>";
+       
+   }
+}
+
+Calculadora($_GET['numero1'], $_GET['numero2'], $_GET['negrita']);
+
+//index.php?numero1=20&numero2=3&negrita=true
